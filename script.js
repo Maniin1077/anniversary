28453aabc55bb1cb96c15fc10efe9b80b10ea179
Maniin1077 <bsmanikanta2004@gmail.com>
2025-05-22 13:06:35 +0530

    Updated anniversary design, music and animations

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
+const quotes = [
+  "You are my today and all of my tomorrows.",
+  "I love you more every single day.",
+  "With you, forever isn’t long enough.",
+  "You’re the best thing I ever waited for.",
+  "Together is a beautiful place to be."
+];
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.remove('active');
-    if (i === index) {
-      slide.classList.add('active');
-    }
-  });
+let index = 0;
+const quoteBox = document.getElementById("quoteBox");
+
+function changeQuote() {
+  quoteBox.style.opacity = 0;
+  setTimeout(() => {
+    quoteBox.innerText = quotes[index];
+    quoteBox.style.opacity = 1;
+    index = (index + 1) % quotes.length;
+  }, 800);
 }
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}
+setInterval(changeQuote, 5000);
 
-setInterval(nextSlide, 3000); // Change every 3 seconds
-showSlide(currentSlide);
+function toggleMusic() {
+  const music = document.getElementById("bgMusic");
+  if (music.paused) music.play();
+  else music.pause();
+}
